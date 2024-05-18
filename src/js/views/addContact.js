175d7af8e7fb6283a223b692c6247803227e685f
Theresa Barkasy <112668448+thatreligionmajor@users.noreply.github.com>
2024-05-18 10:52:40 -0400

    Update addContact.js
    readability space

--- src/js/views/addContact.js
+++ src/js/views/addContact.js
@@ -3,6 +3,7 @@ import { Link, useNavigate } from "react-router-dom";
 import { Context } from "../store/appContext";
 
 const AddContact = () => {
+    
     const{store, actions} = useContext(Context);
     const [fullName, setFullName] = useState("");
     const[email, setEmail] = useState("");
